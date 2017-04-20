07e7ddc83cea6953c7d79e956b71f64c3a278a17
Ferran Torredeflot <ftorredeflot@gmail.com>
2017-04-20 20:40:30 +0200

    inserciones de entidades creadas

--- src/java/beans/NewSessionBean.java
+++ src/java/beans/NewSessionBean.java
@@ -5,8 +5,13 @@
  */
 package beans;
 
-import javax.ejb.Stateless;
 
+import entities.*;
+import java.util.List;
+import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.PersistenceUnit;
 /**
  *
  * @author 47912513V
@@ -14,6 +19,60 @@ import javax.ejb.Stateless;
 @Stateless
 public class NewSessionBean {
 
-    // Add business logic below. (Right-click in editor and choose
-    // "Insert Code > Add Business Method")
+    @PersistenceUnit
+    EntityManagerFactory emf;
+    // insertar usuario
+    public boolean insertUsuario(Usuario u) {
+        if (!existUsuario(u)) {
+            EntityManager em = emf.createEntityManager();
+            em.persist(u);
+            em.flush();  
+            em.close();
+            return true;
+        }
+        return false;
+    }
+    
+        public boolean existUsuario(Usuario u) {
+        EntityManager em = emf.createEntityManager();
+        Usuario encontrado = em.find(Usuario.class, u.getNombreUsuario());
+        em.close();
+        return encontrado != null;
+    }
+  // insertar alquiler
+            public boolean insertAlquiler(Alquiler a) {
+        if (!existAlquiler(a)) {
+            EntityManager em = emf.createEntityManager();
+            em.persist(a);
+            em.flush();  
+            em.close();
+            return true;
+        }
+        return false;
+    }
+    
+        public boolean existAlquiler(Alquiler a) {
+        EntityManager em = emf.createEntityManager();
+        Alquiler encontrado = em.find(Alquiler.class, a.getIdAlquiler());
+        em.close();
+        return encontrado != null;        
+    }
+  //insertar producto
+        public boolean insertProducto(Producto p) {
+        if (!existProducto(p)) {
+            EntityManager em = emf.createEntityManager();
+            em.persist(p);
+            em.flush();  
+            em.close();
+            return true;
+        }
+        return false;
+    }
+    
+        public boolean existProducto(Producto p) {
+        EntityManager em = emf.createEntityManager();
+        Producto encontrado = em.find(Producto.class, p.getIdProducto());
+        em.close();
+        return encontrado != null;
+    }
 }
