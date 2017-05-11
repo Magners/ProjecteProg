8cdf9160ca8e4e2cd974b1e25dab85bfb2170e35
Ferran Torredeflot <ftorredeflot@gmail.com>
2017-05-11 17:19:17 +0200

    login a medias

--- src/java/beans/NewSessionBean.java
+++ src/java/beans/NewSessionBean.java
@@ -5,13 +5,15 @@
  */
 package beans;
 
-
 import entities.*;
+import static java.lang.Boolean.*;
 import java.util.List;
 import javax.ejb.Stateless;
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.PersistenceUnit;
+import javax.persistence.Query;
+
 /**
  *
  * @author 47912513V
@@ -21,58 +23,83 @@ public class NewSessionBean {
 
     @PersistenceUnit
     EntityManagerFactory emf;
+
     // insertar usuario
     public boolean insertUsuario(Usuario u) {
         if (!existUsuario(u)) {
             EntityManager em = emf.createEntityManager();
             em.persist(u);
-            em.flush();  
+            em.flush();
             em.close();
             return true;
         }
         return false;
     }
-    
-        public boolean existUsuario(Usuario u) {
+
+    public boolean existUsuario(Usuario u) {
         EntityManager em = emf.createEntityManager();
         Usuario encontrado = em.find(Usuario.class, u.getNombreUsuario());
         em.close();
         return encontrado != null;
     }
-  // insertar alquiler
-            public boolean insertAlquiler(Alquiler a) {
+    // insertar alquiler
+
+    public boolean insertAlquiler(Alquiler a) {
         if (!existAlquiler(a)) {
             EntityManager em = emf.createEntityManager();
             em.persist(a);
-            em.flush();  
+            em.flush();
             em.close();
             return true;
         }
         return false;
     }
-    
-        public boolean existAlquiler(Alquiler a) {
+
+    public boolean existAlquiler(Alquiler a) {
         EntityManager em = emf.createEntityManager();
         Alquiler encontrado = em.find(Alquiler.class, a.getIdAlquiler());
         em.close();
-        return encontrado != null;        
+        return encontrado != null;
     }
-  //insertar producto
-        public boolean insertProducto(Producto p) {
+    //insertar producto
+
+    public boolean insertProducto(Producto p) {
         if (!existProducto(p)) {
             EntityManager em = emf.createEntityManager();
             em.persist(p);
-            em.flush();  
+            em.flush();
             em.close();
             return true;
         }
         return false;
     }
-    
-        public boolean existProducto(Producto p) {
+
+    public boolean existProducto(Producto p) {
         EntityManager em = emf.createEntityManager();
         Producto encontrado = em.find(Producto.class, p.getIdProducto());
         em.close();
         return encontrado != null;
+    }
+    
+      public boolean existUsn(String p) {
+        EntityManager em = emf.createEntityManager();
+        Usuario encontrado = em.find(Usuario.class, p);
+        em.close();
+        return encontrado != null;
+    }
+
+ public boolean username(String usn) {
+
+        EntityManager em = emf.createEntityManager();
+        
+        Query usuario1=em.createNamedQuery("Usuario.findByNombreUsuario");
+        usuario1.setParameter("nombreUsuario", usn);
+                        
+        Usuario result1 = (Usuario) usuario1.getSingleResult(); 
+        if (result1.getNombreUsuario().equalsIgnoreCase(usn)) {
+           return TRUE;
+        } else {
+            return FALSE;
+        }
     }
 }
